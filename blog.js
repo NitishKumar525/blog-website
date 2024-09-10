96f7f214041c0954f188726194ab392e81c82800
CALCULATOR <nitishjha525@gmail.com>
2024-09-11 01:38:48 +0530

    added PORT env for vercel

--- blog.js
+++ blog.js
@@ -2,15 +2,18 @@ const express = require('express');
 const blogroute = require('./routes/blogroute');
 const app = express();
 
+// Middleware and configurations
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+// Routes
 app.use('/', blogroute);
 app.use('/blog', blogroute);
 
+// Dynamic port assignment
+const PORT = process.env.PORT || 5000;  // Use the port assigned by the environment, default to 5000 locally
 
-
-app.listen(5000, ()=>{
-    console.log("server is running @5000");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
